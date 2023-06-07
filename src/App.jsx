e5b580758fbdb8f32e6933d4588b10d53a8f6463
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2023-06-07 12:12:21 +0200

    include carpet components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
-    const [first, setfirst] = useState(second)
-export const App=()=>{
-    return(
+import React, { useState } from "react";
+import { AddCategory } from "./assets/components/AddCategory";
+
+export const App = () => {
+  const [categories, setCategories] = useState(['Naruto', 'Dragon Ball']);
+
+  const onCategory = (newCategory) => {
+    setCategories([...categories, newCategory]);
+  };
+
+  return (
     <>
-    <h1>Gif App</h1>
+      <h1>Gif App</h1>
+
+      <AddCategory onNewCategory={onCategory} />
+
+      <ol>
+        {categories.map(category => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
-    )
-}
+  );
+};
