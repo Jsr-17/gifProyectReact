a22452262da3ad8a8175c8d72f59b5d176634154
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2023-06-07 16:44:14 +0200

    making the  petitions async

--- src/assets/components/GifGrid.jsx
+++ src/assets/components/GifGrid.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { getGif } from "../helpers/getGifs";
 
 
 export const GifGrid = ({ category}) => {
 
-    getGif(category);
+    const [images,setImages]=useState([])
 
+    const getImages=async()=>{
+        const newImages = await getGif(category);
+        setImages(newImages);
+    }
+
+    useEffect(()=>{
+        getImages();
+
+},[])
     return (
         <>
             <h3>{category}</h3>
